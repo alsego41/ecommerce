19683975a7e46f07be360ae7382b77e80def6860
Alberto González <albertogonzalez.asg@gmail.com>
2021-10-05 08:48:48 -0300

    Initial files and config for publication page

--- controllers/addproduct.php
+++ controllers/addproduct.php
@@ -2,6 +2,7 @@
     require_once "../views/modules/pageHead.php";
     head('Agregar nuevo producto - Administrador');
 ?>
+
 <body>
     <?php 
         require_once "../views/modules/navbar.php";
--- views/modules/publication.php
+++ views/modules/publication.php
@@ -1,16 +1,24 @@
-<div class="publication__main">
+<?php 
+    function renderPublication($product){
+        echo "
+        <div class='publication__main--header'>
+            <div class='publication__images'>
+            </div>
+            <div class='publication__details--head'>
+                <p>$product[productName]</p>
+                <p>$product[productDescription]</p>
+            </div>
+            <div class='publication__cart'>
+                <p>$product[productPrice]</p>
+                <p>Qty</p>
+                <p>$product[productStock]</p>
+            </div>
+        </div>";
+    }
+?>
 
-    <!-- <img src="main.jpg" alt="">
-    <img src="others.jpg" alt=""> -->
+<!-- <img src='main.jpg' alt=''>
+        <img src='others.jpg' alt=''> -->
 
-    <p>Name</p>
-    <p>Description</p>
-</div>
-<div class="publication__cart">
-    <p>Price</p>
-    <p>Available stock</p>
-    <p>Qty</p>
-    <p>Shipping or other ways</p>
-    <p>Buy options - cards, transfers, mp</p>
-
-</div>
+<!-- <p>Buy options - cards, transfers, mp</p>
+                <p>Shipping or other ways</p> -->
