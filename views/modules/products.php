f598f7a3248fac82a4aca71f9576fe3697e74743
Alberto González <albertogonzalez.asg@gmail.com>
2021-10-05 07:42:20 -0300

    Added product listing head

--- views/modules/products.php
+++ views/modules/products.php
@@ -1,8 +1,19 @@
 <?php
+// pasar $qty como arg desde el model a traves del controller
     function renderProduct($products){
         if ($products){
             $qty = mysqli_num_rows($products);
             if ($qty > 0){
+                echo "<div class='products__box products__box--head'>
+                    <p class='products__maintext'>Nombre del producto</p>
+                    <div class='products__actions'>
+                        <p class='products__actions--head'>Acciones</p>
+                    </div>
+                    <div class='products__stock'>
+                        <p class='products__stock--text'>Stock Disponible</p>
+                    </div>
+                    <p class='products__price'>Precio unitario</p>
+                </div>";
                 while ($result = mysqli_fetch_assoc($products)){
                     if ($result['productState'] == 1){
                         $state = 'ACTIVADO';
@@ -16,9 +27,7 @@
                             <p class='products__actions--text'>ELIMINAR</p>
                         </div>
                         <div class='products__stock'>
-                            <p class='products__stock--text'>-</p>
                             <p class='products__stock--text'>$result[productStock]</p>
-                            <p class='products__stock--text'>+</p>
                         </div>
                         <p class='products__price'>$result[productPrice]</p>
                     </div>";
@@ -31,3 +40,5 @@
         }
     }
 ?>
+<!-- <p class='products__stock--text'>+</p>
+            <p class='products__stock--text'>-</p> -->
