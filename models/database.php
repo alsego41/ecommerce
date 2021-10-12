478088b12dffece7ea16ebcd1a3beefde3719504
Alberto González <albertogonzalez.asg@gmail.com>
2021-10-12 09:16:30 -0300

    updated db connection

--- models/database.php
+++ models/database.php
@@ -1,9 +1,10 @@
 <?php   
     function connect(){
-        $host = 'localhost';
-        $user = 'root';
-        $psw = '';
-        $dbname = 'ecommerce';
+        $host = 'remotemysql.com';
+        $user = 'B1McginKUR';
+        $psw = '7C7u7waOgt';
+        $dbname = 'B1McginKUR';
+        $port = 3306;
 
         $connection = mysqli_connect($host, $user, $psw, $dbname);
         return $connection;
