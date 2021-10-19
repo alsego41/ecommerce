fc905d669c936a02be6f06bf5cbdc2c33dea6afb
Alberto González <albertogonzalez.asg@gmail.com>
2021-10-19 14:55:54 -0300

    Empty cart page styled

--- views/emptyCart.php
+++ views/emptyCart.php
@@ -1,1 +1,4 @@
-<p>El carrito esta vacío, vuelve al inicio y agrega nuevos productos</p>
+<div class="cart__container--empty">
+    <p>El carrito esta vacío, vuelve al inicio y agrega nuevos productos</p>
+    <a href="./" class="btn__def"><button>Ir a Inicio</button></a>
+</div>
