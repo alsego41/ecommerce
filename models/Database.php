8134e16256ef254913e372ea7a48c7cce8f4d6da
Alberto González <albertogonzalez.asg@gmail.com>
2021-10-20 16:48:53 -0300

    Updated db credentials to remote

--- models/Database.php
+++ models/Database.php
@@ -1,15 +1,15 @@
 <?php   
     class Database {
-        // public static $host = 'remotemysql.com';
-        // public static $user = 'B1McginKUR';
-        // public static $psw = '7C7u7waOgt';
-        // public static $dbname = 'B1McginKUR';
+        public static $host = 'remotemysql.com';
+        public static $user = 'B1McginKUR';
+        public static $psw = '7C7u7waOgt';
+        public static $dbname = 'B1McginKUR';
         public static $port = 3306;
 
-        public static $host = 'localhost';
-        public static $user = 'root';
-        public static $psw = '';
-        public static $dbname = 'ecommerce';
+        // public static $host = 'localhost';
+        // public static $user = 'root';
+        // public static $psw = '';
+        // public static $dbname = 'ecommerce';
 
         public static function connect() {
             $connection = mysqli_connect(self::$host, self::$user, self::$psw, self::$dbname);
